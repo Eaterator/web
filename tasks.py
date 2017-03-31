3b3d05605aa4d03853e26ad0c2c11925de99865a
Ubuntu <ubuntu@ip-172-31-27-134.ec2.internal>
2017-03-31 00:32:07 +0000

    Working spooler function to update FLICKR images added

--- tasks.py
+++ tasks.py
@@ -1,9 +1,8 @@
 import requests
-import uwsgidecorators
 import uwsgi
 import traceback
+from uwsgidecorators import spoolraw, cron
 from fuzzywuzzy import fuzz
-from uwsgidecorators import cron
 from sqlalchemy import desc, func, not_
 from time import sleep
 from timeit import default_timer
@@ -21,19 +20,19 @@ FLICKR_URL_FORMATTER = "https://api.flickr.com/services/rest/?"\
 FLICKR_SEARCH_METHOD = 'flickr.photos.search'
 FLICKR_RESPONSE_TYPE = 'json'
 FLICKR_WORD_SERACH_SIZE = 3
-CRON_FREQUENCY = 60 * 60  # 1 day
+CRON_FREQUENCY = 60 * 60  # 1 hour
 
 
 # how to setup ini: http://wp.hthieu.com/?p=296
 def update_flickr_images(data):
     try:
+        app.logger.debug("FLICKR | data: {0}".format(str(data)))
         app.logger.debug("FLICKR | running spool function")
-        data = {
-            'pk': int(data[b'pk'].decode('utf-8')), 
-            'title': data[b'title'].decode('utf-8').translate(PUNCTUATION_TRANSLATOR)
-        }
+        data['pk'] = int(data[b'pk'].decode('utf-8'))
+        data['title'] = data[b'title'].decode('utf-8').lower().replace('epicurious', "").replace('.com', '').\
+            translate(PUNCTUATION_TRANSLATOR).strip()
         if not data['title']:
-            return
+            return uwsgi.SPOOL_OK
         title_words_query = '|'.join(data['title'].split())
         top_ingredients = db.session.query(Ingredient).\
             filter(
@@ -51,53 +50,54 @@ def update_flickr_images(data):
                 func.count(Ingredient.name)
             )).limit(FLICKR_WORD_SERACH_SIZE)
         pertinent_recipe_words = " ".join(i.name for i in top_ingredients)
+        if not pertinent_recipe_words:
+            pertinent_recipe_words =  " ".join(data['title'].split()[:3])
+        app.logger.debug("FLICKR |  ingredients: {0}".format(pertinent_recipe_words))
         resp = requests.get(
             FLICKR_URL_FORMATTER.format(
                 FLICKR_SEARCH_METHOD, FLICKR_API_KEY, pertinent_recipe_words, FLICKR_RESPONSE_TYPE
             )
         )
+        app.logger.debug("FLICKR | resp status code: {0}".format(resp.status_code))
         resp = resp.json()
-        start = default_timer()
         try:
             top_photos = sorted(
                 [(fuzz.partial_ratio(p['title'], data["title"]), p["server"], p["id"], p["farm"], p["secret"], p["title"])
                 for p in resp["photos"]["photo"]],
                 reverse=True,
                 key=lambda i: i[0])[:TOPN_FLICKR]
+            app.logger.debug("FLICKR | found x top photos: {0}".format(len(top_photos)))
         except KeyError:
             app.logger.error("FLICKR SPOOLER | Error searching for recipe ({0}): {1}. Words: {2}".format(
                 data["pk"], data["title"], pertinent_recipe_words
             ))
-            sleep(FLICKR_REQUEST_DELAY - (default_timer() - start()))
             return uwsgi.SPOOL_OK
 
         for photo in top_photos:
-            app.logger.debug("FLICKR SPOOLER | Added photo ({0}): {1} ; {2}".format(
-                data["pk"], data["title"], photo[5]
-            ))
             new_recipe_image = RecipeImage(
                 recipe=data["pk"],
                 server_id=photo[1],
                 photo_id=photo[2],
                 farm_id=photo[3],
                 secret=photo[4],
-                title=photo[5],
+                title=photo[5][:100],
                 relevance=photo[0]
             )
             db.session.add(new_recipe_image)
+        app.logger.debug("Committing images to DB")
         db.session.commit()
-        sleep(FLICKR_REQUEST_DELAY - (default_timer() - start))
+        app.logger.debug("FLICKR | Finished spool function")
         return uwsgi.SPOOL_OK
     except:
-        app.logger.error("FLICKR | Unknown error: {0}".format(traceback.format_exc()))
+        app.logger.error("FLICKR | Unknown error, exited spooler: {0}".format(traceback.format_exc()))
         return uwsgi.SPOOL_OK
 
 
 # run every hour at *:30
-@cron(5, -1, -1, -1, -1)
+@cron(-1, -1, -1, -1, -1)
 def get_recipes_without_images(*args):
     app.logger.debug("CRON job called for FLICKR update")
-    maximum_recipes = CRON_FREQUENCY / FLICKR_REQUEST_DELAY
+    maximum_recipes = CRON_FREQUENCY / (FLICKR_REQUEST_DELAY / 1000)
     recipes = Recipe.query.\
         filter(
             not_(
@@ -105,7 +105,8 @@ def get_recipes_without_images(*args):
                     db.session.query(func.distinct(RecipeImage.recipe))
                 )
             )
-        ).all()
+        # ).limit(int(maximum_recipes*0.9)).all()
+        ).limit(55).all()
     app.logger.debug("Number of recipes without images: {0}".format(len(recipes)))
     for recipe in recipes:
         if recipe.title:
